10c1985a6c130028c8491f259d551158942e4adb
Charles Roberts <cjroberts2013@gmail.com>
2022-06-23 22:59:55 -0400

    Initial Commit

---
+++ pinch-app-swiftui/pinch_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinch_app_swiftuiApp.swift
+//  pinch-app-swiftui
+//
+//  Created by Charles Roberts on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct pinch_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
